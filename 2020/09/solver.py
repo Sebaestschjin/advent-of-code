708b065a2d9d50d3a45040d8a31e3f5c7b545d82
Sebastian Stern <sebastian.stern.42@gmail.com>
2020-12-09 13:30:34 +0100

    Adds 2020.09

---
+++ 2020/09/solver.py
@@ -0,0 +1,51 @@
+def is_valid(number, part):
+    # print(number)
+    # print(part)
+    for i in range(len(part)):
+        for j in range(len(part)):
+            if i != j and part[i] + part[j] == number:
+                return True
+    return False
+
+
+def solve_a(data, preamble_size):
+    for i in range(preamble_size, len(data)):
+        number = data[i]
+        start = i - preamble_size
+        end = i
+        if not is_valid(number, data[start:end]):
+            return number
+
+
+def solve_b(data, preamble_size):
+    invalid_number = solve_a(data, preamble_size)
+
+    for i in range(len(data)):
+        total = data[i]
+        for j in range(i + 1, len(data)):
+            total += data[j]
+            if total == invalid_number:
+                min_number = min(data[i:j])
+                max_number = max(data[i:j])
+                return min_number + max_number
+            if total > invalid_number:
+                break
+
+
+def read(filename='in'):
+    with open(filename, 'r') as file:
+        return [int(line) for line in file.readlines()]
+
+
+def run():
+    puzzle = read()
+
+    solution_a = solve_a(puzzle, 25)
+    print(solution_a)
+
+    solution_b = solve_b(puzzle, 25)
+    print(solution_b)
+
+
+if __name__ == '__main__':
+    run()
--- 2020/template/test_solver.py
+++ 2020/template/test_solver.py
@@ -8,20 +8,20 @@ class Test(unittest.TestCase):
     def test_example_a(self):
         puzzle = []
         result = solver.solve_a(puzzle)
-        assert_that(result).is_equal_to(2)
+        # assert_that(result).is_equal_to(2)
 
     def test_solution_a(self):
         result = solver.solve_a(solver.read())
-        assert_that(result).is_equal_to(416)
+        # assert_that(result).is_equal_to(416)
 
     def test_example_b(self):
         puzzle = []
         result = solver.solve_b(puzzle)
-        assert_that(result).is_equal_to(1)
+        # assert_that(result).is_equal_to(1)
 
     def test_solution_b(self):
         result = solver.solve_b(solver.read())
-        assert_that(result).is_equal_to(688)
+        # assert_that(result).is_equal_to(688)
 
 
 if __name__ == '__main__':
