8a496380935091a872fd13b68fe147d25c047f33
Sebastian Stern <sebastian.stern.42@gmail.com>
2020-12-10 10:23:38 +0100

    Adds the name of the Day to the folders

--- 2020/10 - Adapter Array/test_reader.py
+++ 2020/10 - Adapter Array/test_reader.py
@@ -6,7 +6,7 @@ import reader
 
 class Test(unittest.TestCase):
     def test_example(self):
-        lines = ['16\n', '10\n', '15\n', '5\n', '1\n', '11\n', '7\n', '19\n', '6\n', '12\n', '4\n']
+        lines = ['16\n', '10 - Adapter Array\n', '15\n', '5\n', '1\n', '11\n', '7\n', '19\n', '6\n', '12\n', '4\n']
         result = reader.read_lines(lines)
         assert_that(result).is_equal_to([16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4])
 
