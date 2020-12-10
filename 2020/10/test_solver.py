0ad51dea5ca894f026a0dbec179df57c341840e1
Sebastian Stern <sebastian.stern.42@gmail.com>
2020-12-10 09:09:45 +0100

    Adds 2020.10

---
+++ 2020/10/test_solver.py
@@ -0,0 +1,40 @@
+import unittest
+from assertpy import assert_that
+
+import solver
+
+
+class Test(unittest.TestCase):
+    def test_example_a_1(self):
+        puzzle = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+        result = solver.solve_a(puzzle)
+        assert_that(result).is_equal_to(35)
+
+    def test_example_a_2(self):
+        puzzle = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4,
+                  2, 34, 10, 3]
+        result = solver.solve_a(puzzle)
+        assert_that(result).is_equal_to(220)
+
+    def test_solution_a(self):
+        result = solver.solve_a(solver.read())
+        assert_that(result).is_equal_to(1755)
+
+    def test_example_b_1(self):
+        puzzle = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+        result = solver.solve_b(puzzle)
+        assert_that(result).is_equal_to(8)
+
+    def test_example_b_2(self):
+        puzzle = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4,
+                  2, 34, 10, 3]
+        result = solver.solve_b(puzzle)
+        assert_that(result).is_equal_to(19208)
+
+    def test_solution_b(self):
+        result = solver.solve_b(solver.read())
+        assert_that(result).is_equal_to(4049565169664)
+
+
+if __name__ == '__main__':
+    unittest.main()
